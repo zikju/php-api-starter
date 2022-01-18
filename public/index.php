4e48854df1b9628ed2e97dd6f27e2495bab1d263
zikju <zqzer@zq.lt>
2022-01-18 14:34:26 +0200

    refactor(core): move index.php to public/ folder

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // Initialize first-place loaders
-require __DIR__ .'/init.php';
+require __DIR__ .'/../init.php';
 
 // Initialize Routes handler (API endpoints)
-require __DIR__ .'/router.php';
+require __DIR__ .'/../router.php';
