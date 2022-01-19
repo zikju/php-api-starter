b612de85e26a1a18e9f4b5a06d2dbf08f7082939
zikju <zqzer@zq.lt>
2022-01-19 13:59:38 +0200

    feat(Http\Request): add getData() method
    Returns request body or query data

--- classes/Http/Request.php
+++ classes/Http/Request.php
@@ -16,6 +16,36 @@ class Request
     }
 
 
+    /** Returns incoming request body or query data
+     * @return array|string
+     */
+    public static function getData()
+    {
+        $method = self::getMethod();
+
+        // 'GET' and 'DELETE' methods
+        if ($method === 'GET' || $method === 'DELETE') {
+            return $_GET;
+        }
+
+        if(!empty($_POST)) {
+            // when using 'application/x-www-form-urlencoded' or 'multipart/form-data'
+            // as the HTTP Content-Type in the request.
+            // NOTE: if this is the case and $_POST is empty,
+            // check the variables_order in php.ini! - it must contain the letter P
+            return $_POST;
+        }
+
+        // when using application/json as the HTTP Content-Type in the request
+        $post = json_decode(file_get_contents('php://input'), true);
+        if(json_last_error() == JSON_ERROR_NONE) {
+            return $post;
+        }
+
+        return [];
+    }
+
+
     /** Check if incoming request method is allowed
      * @return bool
      */
