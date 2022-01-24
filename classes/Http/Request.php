a78750e710de814cc7d6c01bbb8c75fb13b3f2a0
zikju <zqzer@zq.lt>
2022-01-24 10:30:18 +0200

    refactor(Http\Request): rewrite whole code

--- classes/Http/Request.php
+++ classes/Http/Request.php
@@ -1,13 +1,46 @@
 <?php
+declare(strict_types=1);
 
 namespace zikju\Shared\Http;
 
+
+
 class Request
 {
-    public static array $allowedList = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'];
+    /**
+     * List of allowed Methods
+     *
+     * header("Access-Control-Allow-Method..."
+     */
+    private static array $ALLOWED_METHODS_LIST = [
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE',
+        'OPTIONS'
+    ];
+
+    /**
+     * List of allowed Headers
+     *
+     * header("Access-Control-Allow-Headers..."
+     */
+    private static array $ALLOWED_HEADERS_LIST = [
+        'Origin',
+        'Content-Type',
+        'Authorization',
+        'Accept',
+        'X-Requested-With',
+        'Content-Disposition',
+        'X-Refresh-Token',
+        'X-Fingerprint'
+    ];
 
 
-    /** Get incoming request method name
+
+    /**
+     * Get incoming request method name
+     *
      * @return string
      */
     public static function getMethod(): string
@@ -16,7 +49,57 @@ class Request
     }
 
 
-    /** Returns incoming request body or query data
+
+    /**
+     * Returns list of allowed methods
+     *
+     * @return array|string[]
+     */
+    public static function getAllowedMethods (): array
+    {
+        return self::$ALLOWED_METHODS_LIST;
+    }
+
+
+
+    /**
+     * Returns list of allowed headers
+     *
+     * @return array|string[]
+     */
+    public static function getAllowedHeaders (): array
+    {
+        return self::$ALLOWED_HEADERS_LIST;
+    }
+
+
+
+    /**
+     * Set CORS Privacy header settings
+     */
+    public static function setHeaders (): void
+    {
+        /**
+         * IMPORTANT:
+         * If you are using: "Access-Control-Allow-Credentials" = true
+         * make sure that "Access-Control-Allow-Origin" is not "*",
+         * it must be set with a proper domain!
+         * (a lot of blood was spilled here :/ )
+         * */
+        // header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: " . implode(', ', self::getAllowedMethods()));
+        header("Access-Control-Allow-Headers: " . implode(', ', self::getAllowedHeaders()));
+        header("Access-Control-Max-Age: 60");
+        header("Content-Type: application/json; charset=UTF-8");
+    }
+
+
+
+    /**
+     * Returns incoming request body or query data
+     *
      * @return array|string
      */
     public static function getData()
@@ -46,15 +129,32 @@ class Request
     }
 
 
-    /** Check if incoming request method is allowed
+
+    /**
+     * Check if incoming request method is allowed
+     */
+    public static function validateIncomingMethod (): void
+    {
+        if (!self::isMethodAllowed()) {
+            Response::send(
+                'error',
+                'REQUEST METHOD IS INVALID'
+            );
+        }
+    }
+
+
+
+    /**
+     * Check if incoming request method is allowed
+     *
      * @return bool
      */
-    public static function isAllowed(): bool
+    private static function isMethodAllowed(): bool
     {
         return (in_array(
             Request::getMethod(),
-            Request::$allowedList
+            Request::getAllowedMethods()
         ));
     }
-
 }
