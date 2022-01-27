3d478256f69f41cb0e9d30a40100832306918446
zikju <zqzer@zq.lt>
2022-01-27 10:36:58 +0200

    feat(Util\Ip): add new class to get client ip address

---
+++ classes/Util/Ip.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+
+namespace zikju\Shared\Util;
+
+
+class Ip
+{
+    /**
+     * Returns client IP Address
+     * @return string
+     */
+    public static function get_client_ip (): string
+    {
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipAddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipAddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipAddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipAddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipAddress = 'UNKNOWN';
+
+        return $ipAddress;
+    }
+}
