a58ffadbb230cbc653933812835c131fc78b337c
zikju <zqzer@zq.lt>
2022-01-31 14:25:58 +0200

    fix(Request): rename 'Request' namespace

--- app/Auth/LoginController.php
+++ app/Auth/LoginController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace zikju\Endpoint\Auth;
 
 
-use zikju\Shared\Http\Request;
+use zikju\Shared\Http\Request\Request;
 use zikju\Shared\Http\Response;
 use zikju\Shared\Util\JwtToken;
 use zikju\Shared\Util\Password;
