d396659741bb207da8a896d4644d7d6d85618484
zikju <zqzer@zq.lt>
2022-02-02 13:04:44 +0200

    refactor(Request): move getData() method to Request.php

--- classes/Http/Request/Request.php
+++ classes/Http/Request/Request.php
@@ -7,5 +7,41 @@ namespace zikju\Shared\Http\Request;
 
 class Request extends RequestHeader
 {
+    /**
+     * Returns incoming request body or query data
+     *
+     * @return array|string
+     */
+    public static function getData()
+    {
+        $method = self::getMethod();
 
+        // 'GET' and 'DELETE' methods
+        if ($method === 'GET' || $method === 'DELETE') {
+            return $_GET;
+        }
+
+        // 'PUT' method
+        if ($method === 'PUT') {
+            parse_str(file_get_contents('php://input'), $put_data);
+            return $put_data;
+        }
+
+        // 'POST' method
+        if($method === 'POST' && !empty($_POST)) {
+            // when using 'application/x-www-form-urlencoded' or 'multipart/form-data'
+            // as the HTTP Content-Type in the request.
+            // NOTE: if this is the case and $_POST is empty,
+            // check the variables_order in php.ini! - it must contain the letter P
+            return $_POST;
+        }
+
+        // when using application/json as the HTTP Content-Type in the request
+        $post = json_decode(file_get_contents('php://input'), true);
+        if(json_last_error() == JSON_ERROR_NONE) {
+            return $post;
+        }
+
+        return [];
+    }
 }
--- classes/Http/Request/RequestMethod.php
+++ classes/Http/Request/RequestMethod.php
@@ -49,46 +49,6 @@ class RequestMethod
 
 
     /**
-     * Returns incoming request body or query data
-     *
-     * @return array|string
-     */
-    public static function getData()
-    {
-        $method = self::getMethod();
-
-        // 'GET' and 'DELETE' methods
-        if ($method === 'GET' || $method === 'DELETE') {
-            return $_GET;
-        }
-
-        // 'PUT' method
-        if ($method === 'PUT') {
-            parse_str(file_get_contents('php://input'), $put_data);
-            return $put_data;
-        }
-
-        // 'POST' method
-        if($method === 'POST' && !empty($_POST)) {
-            // when using 'application/x-www-form-urlencoded' or 'multipart/form-data'
-            // as the HTTP Content-Type in the request.
-            // NOTE: if this is the case and $_POST is empty,
-            // check the variables_order in php.ini! - it must contain the letter P
-            return $_POST;
-        }
-
-        // when using application/json as the HTTP Content-Type in the request
-        $post = json_decode(file_get_contents('php://input'), true);
-        if(json_last_error() == JSON_ERROR_NONE) {
-            return $post;
-        }
-
-        return [];
-    }
-
-
-
-    /**
      * Check if incoming request method is allowed
      */
     public static function validateIncomingMethod (): void
