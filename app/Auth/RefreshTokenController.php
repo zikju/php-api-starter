d27a3675e4e6c13ac6689e3e7980eeb94d4933db
zikju <zqzer@zq.lt>
2022-02-02 17:27:57 +0200

    feat(RefreshTokenController): add class to handle 'Refresh Token'

---
+++ app/Auth/RefreshTokenController.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+
+namespace zikju\Endpoint\Auth;
+
+
+use zikju\Shared\Http\Response;
+use zikju\Shared\Util\JwtToken;
+
+class RefreshTokenController extends UserSession
+{
+
+    public function refresh ()
+    {
+        // Create Refresh Token (user session in database)
+        $this->refreshUserSession();
+        $refresh_token = $this->getRefreshToken();
+
+        // Create Access Token (JWT)
+        $access_token_payload = $this->getUserPayload();
+        $access_token = JwtToken::create($access_token_payload);
+
+        // Send response result
+        $responseArray = [
+            'access_token' => $access_token,
+            'refresh_token' => $refresh_token
+        ];
+
+        Response::sendOk(
+            'Token successfully refreshed',
+            $responseArray
+        );
+    }
+
+
+}
